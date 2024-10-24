cb71a41bde308e3e153502f470398538eff712d5
sivertsj10 <92299801+sivertsj10@users.noreply.github.com>
2024-10-24 17:23:41 +0200

    løst M07 igjen

--- teknologi/løype0.py
+++ teknologi/løype0.py
@@ -4,30 +4,36 @@ from chewbacca import Chewbacca
 
 def run():
     chewie = Chewbacca()
-
-    v = chewie.drive_gyro_dist(start_speed= 0, speed= 150, end_speed= 150,
-                               target_distance= 111, target_angle= 0,
+     
+    v = chewie.drive_gyro_dist(start_speed= 0, speed= 300, end_speed= 300,
+                               target_distance= 95, target_angle= 0,
                                stop_at_end= False)
 
-    v = chewie.drive_gyro_turn(start_speed= v, speed= 150, end_speed= 150, 
-                               turn_radius= 250, turn_angle= 75,
+    v = chewie.drive_gyro_turn(start_speed= v, speed= 300, end_speed= 300, 
+                               turn_radius= 250, turn_angle= 70,
                                stop_at_end= False)
     
-    v = chewie.drive_gyro_dist(start_speed= v, speed= 400, end_speed= 0,
-                               target_distance= 173, target_angle= -79,
+    v = chewie.drive_gyro_dist(start_speed= v, speed= 300, end_speed= 0,
+                               target_distance= 192, target_angle= 80,
                                stop_at_end= True)
     
-    
-    
-    # v = chewie.drive_gyro_turn(start_speed= 0, speed= 300, end_speed= 300, 
-    #                            turn_radius= 300, turn_angle= 55,
-    #                            turn_right= True,  stop_at_end= False)
-    
-    # v = chewie.drive_gyro_dist(start_speed= v, speed= 300, end_speed= 0,
-    #                            target_distance= 290, target_angle= -67,
+    chewie.work_motor_L(speed= -500, target_rotation= 250)
+
+    chewie.motor_L.run_angle(40, -10)  #Venstre hjul litt bak
+    chewie.work_motor_L(speed= -500, target_rotation= 1080)
+
+    v = chewie.drive_gyro_dist(start_speed= 0, speed= 100, end_speed= 0,
+                               target_distance= -100, target_angle= 90,
+                               stop_at_end= True)
+
+    chewie.work_motor_L(speed= 500, target_rotation= 1330)
+
+
+
+
+    # v = chewie.drive_gyro_dist(start_speed= v, speed= 200, end_speed= 0,
+    #                            target_distance= -200, target_angle= -79,
     #                            stop_at_end= True)
     
-    chewie.work_motor_L(speed= -800, target_rotation= 1310)
-    chewie.work_motor_L(speed= 800, target_rotation= 1310)
 
-
+ 
