0d4fa16fb8ce05362e3a31e42dbcee5b83cdbdf1
sivertsj10 <92299801+sivertsj10@users.noreply.github.com>
2024-10-25 18:32:41 +0200

    løst M01 og M02

--- teknologi/løype0.py
+++ teknologi/løype0.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env pybricks-micropython
 from chewbacca import Chewbacca
+import time
 
 
 def run():
@@ -14,28 +15,51 @@ def run():
                                stop_at_end= False)
     
     v = chewie.drive_gyro_dist(start_speed= v, speed= 300, end_speed= 0,
-                               target_distance= 192, target_angle= 80,
+                               target_distance= 202, target_angle= 80,
                                stop_at_end= True)
     
     chewie.work_motor_L(speed= -500, target_rotation= 250)
 
     chewie.motor_L.run_angle(40, -10)  #Venstre hjul litt bak
-    chewie.work_motor_L(speed= -500, target_rotation= 1080)
+    chewie.work_motor_L(speed= -500, target_rotation= 1055)
+
+    #time.sleep(1)
 
     v = chewie.drive_gyro_dist(start_speed= 0, speed= 100, end_speed= 0,
-                               target_distance= -100, target_angle= 90,
-                               stop_at_end= True)
-
-    chewie.work_motor_L(speed= 500, target_rotation= 1330)
-
-    v = chewie.drive_gyro_turn(start_speed= 0, speed= 100, end_speed= 0, 
-                               turn_radius= 180, turn_angle= -180,
+                               target_distance= -110, target_angle= 90,
                                stop_at_end= True)
 
 
-    # v = chewie.drive_gyro_dist(start_speed= v, speed= 200, end_speed= 0,
-    #                            target_distance= -200, target_angle= -79,
-    #                            stop_at_end= True)
+    v = chewie.drive_gyro_turn(start_speed= 0, speed= 100, end_speed= 100, 
+                               turn_radius= chewie.WHEEL_DISTANSE / 2, turn_angle= -80,
+                               stop_at_end= True)
+
+    v = chewie.drive_gyro_dist(start_speed= v, speed= 100, end_speed= 100,
+                               target_distance= 120, target_angle= 0,
+                               stop_at_end= True)
+
+    v = chewie.drive_gyro_turn(start_speed= v, speed= 100, end_speed= 0, 
+                               turn_radius= chewie.WHEEL_DISTANSE / 2, turn_angle= -82,
+                               stop_at_end= True)
+
+    # snur korallknoppende opp (M01)
+    v = chewie.drive_gyro_dist(start_speed= v, speed= 70, end_speed= 0,
+                               target_distance= 68, target_angle= -90,
+                               stop_at_end= False)
     
 
+    v = chewie.drive_gyro_dist(start_speed= v, speed= -100, end_speed= 0,
+                               target_distance= 170, target_angle= -90,
+                               stop_at_end= True)
+
+    v = chewie.drive_gyro_turn(start_speed= v, speed= 100, end_speed= 0, 
+                               turn_radius= chewie.WHEEL_DISTANSE / 2, turn_angle= 35,
+                               stop_at_end= True, turn_right= True)
+
+
+    v = chewie.drive_gyro_dist(start_speed= v, speed= 100, end_speed= 0,
+                               target_distance= 215, target_angle= -50,
+                               stop_at_end= True)
+    
+    chewie.work_motor_L(speed= 500, target_rotation= 1330)
  
