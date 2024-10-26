3b7e71d4343625437b35fabfeb0b41a4e8a4a59b
sivertsj10 <92299801+sivertsj10@users.noreply.github.com>
2024-10-27 01:29:22 +0200

    testet "set_known_heading"

--- teknologi/løype1.py
+++ teknologi/løype1.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env pybricks-micropython
 from chewbacca import Chewbacca
+import time
 
 
 def run():
@@ -8,13 +9,28 @@ def run():
 
     fast = 300
     slow = 75
-    v = chewie.drive_gyro_dist(start_speed= 0, speed= slow, end_speed= 0,
-                               target_distance= 700, target_angle= 0,
+    v = chewie.drive_gyro_dist(start_speed= 0, speed= slow, end_speed= slow,
+                               target_distance= 100, target_angle= 0,
                                stop_at_end= True)
     
+    v = chewie.drive_gyro_turn(start_speed= v, speed= slow, end_speed= 0, 
+                               turn_radius= 150, turn_angle= -90,
+                                stop_at_end= False)
+    
     v = chewie.drive_gyro_dist(start_speed= 0, speed= slow, end_speed= 0,
-                               target_distance= -700, target_angle= 0,
+                               target_distance= -250, target_angle= -90,
                                stop_at_end= True)
+    print("Gyro før cal:", chewie.gyro_correction)
+    chewie.press_robot_frem(-45)
+    time.sleep(5)
+    chewie.set_known_heading(-90)
+    chewie.press_robot_frem_ferdig()
+    print("Gyro etter cal:", chewie.gyro_correction)
+
+    v = chewie.drive_gyro_dist(start_speed= 0, speed= slow, end_speed= 0,
+                               target_distance= 500, target_angle= -90,
+                               stop_at_end= True)
+    
     
     # v = chewie.drive_gyro_turstart_speed= v, speed= slow, end_speed= slow, 
     #                            turn_radius= 0, turn_angle= -160,
