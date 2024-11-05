4266047d945a9a0cef38215e32a6841770b02e93
Wiljaisabell <wilja.antonsen@gmail.com>
2024-11-05 17:07:28 +0100

    Oppdateringer på M14

--- teknologi/m14.py
+++ teknologi/m14.py
@@ -16,15 +16,15 @@ def run():
                             target_distance=230, target_angle= -20,
                             stop_at_end= False)
     #kjører 200 mm rett frem
-    v = che.drive_gyro_dist(start_speed= 250, speed=250, end_speed=0,
+    v = che.drive_gyro_dist(start_speed= 250, speed=250, end_speed=190,
                             target_distance=210, target_angle= -15,
-                            stop_at_end= True)
-    
+                            stop_at_end= False)
+        
     #svinger 6o grader til høyre
-    # che.drive_gyro_dist(190,-60,0)
-    # v = che.drive_gyro_dist(start_speed= 190, speed=250, end_speed= 190,
-    #                         target_distance=210, target_angle= -60,
-    #                         stop_at_end= False)
+    che.drive_gyro_dist(190,-60,0)
+    v = che.drive_gyro_dist(start_speed= 190, speed=250, end_speed= 190,
+                            target_distance=210, target_angle= -60,
+                            stop_at_end= False)
     
     # #che.work_motor_L(200,-65,190)
     # #che.drive_gyro_dist(100, -60, 490)
